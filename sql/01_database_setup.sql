e6e7a2d13fe8745166deb4c6a5e4d5e20e9c8e34
shubham negi <shubhamnegi022006@gmail.com>
2026-07-15 18:58:30 +0530

    Update 01_database_setup.sql

--- sql/01_database_setup.sql
+++ sql/01_database_setup.sql
@@ -1,15 +1,27 @@
 -- =====================================================
--- Project: Retail Sales Analysis
--- Database Setup
+-- Project: Retail Sales Analysis Using MySQL
+-- File: 01_database_setup.sql
 -- Author: Shubham negi
+-- Purpose: Database Setup & Verification
 -- =====================================================
 
--- Create Database
-CREATE DATABASE retail_sales;
+-- Create Database (if it doesn't already exist)
+CREATE DATABASE IF NOT EXISTS retail_sales_db;
 
--- Use Database
-USE retail_sales;
+-- Select Database
+USE retail_sales_db;
 
--- View all records
+-- Show all tables
+SHOW TABLES;
+
+-- Preview the dataset
 SELECT *
-FROM retail_sales;
+FROM retail_sales_dataset
+LIMIT 10;
+
+-- Display table structure
+DESCRIBE retail_sales_dataset;
+
+-- Count total records
+SELECT COUNT(*) AS total_records
+FROM retail_sales_dataset;
