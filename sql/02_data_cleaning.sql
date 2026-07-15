1d78c31a4d9a25058e4fc7d7dca297c07bea5ee0
shubham negi <shubhamnegi022006@gmail.com>
2026-07-15 18:56:33 +0530

    Update 02_data_cleaning.sql

--- sql/02_data_cleaning.sql
+++ sql/02_data_cleaning.sql
@@ -1,75 +1,53 @@
 -- =====================================================
 -- Project: Retail Sales Analysis Using MySQL
 -- File: 02_data_cleaning.sql
--- Purpose: Data Quality Assessment
 -- =====================================================
 
 USE retail_sales_db;
 
--- =====================================================
--- 1. Total Records
--- =====================================================
-
+-- Total Records
 SELECT COUNT(*) AS total_records
 FROM retail_sales_dataset;
 
--- =====================================================
--- 2. Check for Duplicate Transactions
--- =====================================================
-
+-- Duplicate Transactions
 SELECT
-    Transaction_ID,
+    `Transaction ID`,
     COUNT(*) AS duplicate_count
 FROM retail_sales_dataset
-GROUP BY Transaction_ID
+GROUP BY `Transaction ID`
 HAVING COUNT(*) > 1;
 
--- =====================================================
--- 3. Check for Missing Values
--- =====================================================
-
+-- Missing Values
 SELECT *
 FROM retail_sales_dataset
 WHERE
-    Transaction_ID IS NULL
+    `Transaction ID` IS NULL
     OR Date IS NULL
-    OR Customer_ID IS NULL
+    OR `Customer ID` IS NULL
     OR Gender IS NULL
     OR Age IS NULL
-    OR Product_Category IS NULL
+    OR `Product Category` IS NULL
     OR Quantity IS NULL
-    OR Price_per_Unit IS NULL
-    OR Total_Amount IS NULL;
+    OR `Price per Unit` IS NULL
+    OR `Total Amount` IS NULL;
 
--- =====================================================
--- 4. Check for Invalid Age
--- =====================================================
-
+-- Invalid Age
 SELECT *
 FROM retail_sales_dataset
 WHERE Age < 0
    OR Age > 100;
 
--- =====================================================
--- 5. Check for Invalid Quantity
--- =====================================================
-
+-- Invalid Quantity
 SELECT *
 FROM retail_sales_dataset
 WHERE Quantity <= 0;
 
--- =====================================================
--- 6. Check for Negative Price
--- =====================================================
-
+-- Invalid Price
 SELECT *
 FROM retail_sales_dataset
-WHERE Price_per_Unit <= 0;
+WHERE `Price per Unit` <= 0;
 
--- =====================================================
--- 7. Check for Negative Total Amount
--- =====================================================
-
+-- Invalid Total Amount
 SELECT *
 FROM retail_sales_dataset
-WHERE Total_Amount <= 0;
+WHERE `Total Amount` <= 0;
