83a12c9a37ed848521bdc271709d204ed3e2cecb
shubham negi <shubhamnegi022006@gmail.com>
2026-07-15 17:46:19 +0530

    Update 01_database_setup.sql

--- sql/01_database_setup.sql
+++ sql/01_database_setup.sql
@@ -1,1 +1,16 @@
 
+-- =====================================================
+-- Project: Retail Sales Analysis
+-- Database Setup
+-- Author: Kritika Thakur
+-- =====================================================
+
+-- Create Database
+CREATE DATABASE retail_sales;
+
+-- Use Database
+USE retail_sales;
+
+-- View all records
+SELECT *
+FROM retail_sales;
